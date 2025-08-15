cbdf5f726e10d5a470c0a7c2c69d9e685f38d17f
aburtasenkov <aburtasenkov@gmail.com>
2025-08-15 21:37:18 +0200

    Reformed code to be ready for input of .mp4 files

--- main.c
+++ main.c
@@ -6,6 +6,29 @@
 #define IMAGE_PATH_ARGV_INDEX 1
 #define BLOCKSIZE_ARGV_INDEX 2
 
+#define REGEX_FILE_EXTENSION_PATTERN "\\.([^.]+)$"
+#define FFMPEG_DECOMPOSE_VIDEO "ffmpeg -i %s -r 24 frames/frame_%04d.png"
+
+void printImage(const char * filename, int blockSize) {
+  // Load image
+  int width, height, Ncomponents;
+  unsigned char * image = stbi_load(filename, &width, &height, &Ncomponents, 0);
+  if (!image) {
+    printf("Error loading image. Aborting...\n");
+    return;
+  }
+
+  printf("Read image width:%d Height:%d ComponentsSize:%d\n", width, height, Ncomponents);
+  char * asciiImage = createAsciiImage(image, width, height, Ncomponents, blockSize);
+  if (!asciiImage) {
+    printf("Error converting image to ascii. Aborting...\n");
+    return;
+  }
+  printf("%s\n", asciiImage);
+
+  stbi_image_free(image);
+}
+
 int main(int argc, char ** argv) {
   /*-----------------------Load terminal input parameters-----------------------*/
   if (argc < ARGC_MIN) {
@@ -15,14 +38,6 @@ int main(int argc, char ** argv) {
 
   // First argument is the path of the pic which will be generated into ascii characters
   char * filename = argv[IMAGE_PATH_ARGV_INDEX];
-  
-  // Load image
-  int width, height, Ncomponents;
-  unsigned char * image = stbi_load(filename, &width, &height, &Ncomponents, 0);
-  if (!image) {
-    printf("Error loading image. Aborting...\n");
-    return 1;
-  }
 
   // Second argument is size of blocksize
   // default: 1 --> 1 pixel = 1 character
@@ -34,16 +49,10 @@ int main(int argc, char ** argv) {
     printf("Pre-condition: blockSize variable must be positive. Aborting...\n");
     return 1;
   }
-
+  
   /*-----------------------Loading done-----------------------*/
 
-  printf("Read image width:%d Height:%d ComponentsSize:%d\n", width, height, Ncomponents);
-  char * asciiImage = createAsciiImage(image, width, height, Ncomponents, blockSize);
-  if (!asciiImage) {
-    return 1;
-  }
-  printf("%s\n", asciiImage);
+  printImage(filename, blockSize);
 
-  stbi_image_free(image);
   return 0;
 }
