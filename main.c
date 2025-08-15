2d15907814c488169c58e202a294a3d1e16b29e1
aburtasenkov <aburtasenkov@gmail.com>
2025-08-15 04:15:00 +0200

    Added unallowed file types

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 
-// currently only supporting PNG data format
-#define STBI_ONLY_PNG
+// Allow only certain data files
+
+// #define STBI_NO_JPEG
+// #define STBI_NO_PNG
+#define STBI_NO_BMP
+#define STBI_NO_PSD
+#define STBI_NO_TGA
+#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM   // (.ppm and .pgm)
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
