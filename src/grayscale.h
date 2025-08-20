813bf10e44e208f8e12e4690ac27d837580d5285
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2025-08-20 12:54:48 +0200

    moved source code into .c file

--- src/grayscale.h
+++ src/grayscale.h
@@ -1,15 +1,5 @@
 #pragma once
 
-#include <limits.h>
-
 #define ERROR_GRAYSCALE_VAL 'E'
 
-unsigned char grayscaleToChar(unsigned char grayScaleValue) {
-  static const char const * Ascii = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
-  static const int AsciiSize = 70; // size of Ascii Array
-
-  // flatten [0:255] values on [0:69] ids
-  unsigned char idx = AsciiSize - 1 - grayScaleValue * AsciiSize / UCHAR_MAX;
-
-  return Ascii[idx];
-}
+unsigned char grayscaleToChar(unsigned char);
