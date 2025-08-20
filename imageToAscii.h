63334bc5ff59ac874e97a74a113eae55f02198e5
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2025-08-20 12:42:55 +0200

    Allowed other file types, not only jpg and png

--- imageToAscii.h
+++ imageToAscii.h
@@ -2,18 +2,6 @@
 
 #include "grayscale.h"
 
-// Allow only certain data files
-
-// #define STBI_NO_JPEG
-// #define STBI_NO_PNG
-#define STBI_NO_BMP
-#define STBI_NO_PSD
-#define STBI_NO_TGA
-#define STBI_NO_GIF
-#define STBI_NO_HDR
-#define STBI_NO_PIC
-#define STBI_NO_PNM   // (.ppm and .pgm)
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
