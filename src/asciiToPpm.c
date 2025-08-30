8aee812eec770711fd99e8d5c9b9f13b6af7a0e2
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2025-08-30 23:07:27 +0200

    Worked forward

--- src/asciiToPpm.c
+++ src/asciiToPpm.c
@@ -1,9 +1,31 @@
 #include "asciiToPpm.h" 
 
 #include "error.h"
+#include "Glyph.h"
 
 #include <stdlib.h>
 #include <stdio.h>
+
+int countRows(const char * ascii) {
+    if (!ascii) printCriticalError(ERROR_BAD_ARGUMENTS, "ascii is null pointer");
+
+    int count = 0;
+    while (*(ascii++) != '\0') {
+        if (*ascii == '\n') ++count;
+    }
+    return ++count;
+}
+
+int countColumns(const char * ascii) {
+    if (!ascii) printCriticalError(ERROR_BAD_ARGUMENTS, "ascii is null pointer");
+
+    int count = 0;
+    while (*(ascii++) != '\0') {
+        if (*ascii == '\n') break; 
+        ++count;
+    }
+    return ++count;
+}
 
 char * ppmHeader(const char * ascii)
 // return PPM file header that will be written to PPM file
@@ -25,15 +47,29 @@ char * ppmHeader(const char * ascii)
     return NULL;
 }
 
+void setPixel(unsigned char * image, int rowSize, int x, int y, const uint8_t * rgb) {
+    // pre conditions
+    
+    int index = (y * rowSize + x) * RGB_CHANNELS;
+    image[index + 0] = rgb[0];
+    image[index + 1] = rgb[1];
+    image[index + 2] = rgb[2];
+}
+
 void asciiToPpm(const char * ascii, const char * filepath) 
 // write ascii characters in PPM file format to filepath
 {
     if (!ascii) printCriticalError(ERROR_BAD_ARGUMENTS, "ascii is null pointer");
     if (!filepath) printCriticalError(ERROR_BAD_ARGUMENTS, "filepath is null pointer");
 
-    // File * ppm = fopen(filename);
-    // if (!ppm) printCriticalError(ERROR_BAD_ARGUMENTS, "Bad filepath [filename: %s]", filename);
+    printf("%d\n", countRows(ascii));
+    printf("%d\n", countColumns(ascii));
 
+    int imageSizeX = GLYPH_W * countColumns(ascii);
+    int imageSizeY = GLYPH_H  * countRows(ascii);
+    int imageSize = imageSizeX * imageSizeY * RGB_CHANNELS;
+    unsigned char * image = (unsigned char *)malloc(imageSize);
 
-    
+    // initialize all pixels to black
+    for (int i = 0; i < imageSize; ++i) image[i] = 0;
 }
