8c07482e95d74b48628b9cf4a9f480951e148eec
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2025-09-09 00:23:14 +0200

    Added output to txt files

--- src/writeFile.c
+++ src/writeFile.c
@@ -7,8 +7,6 @@
 
 #include <string.h>
 
-const char const * supportedOutputFormats[] = {"ppm"};
-
 void writeImage(const char * filepath, const char * outputPath, int blockSize)
 // convert image at filepath to ascii and write the resulting image in outputPath
 // if file format is not supported, function will cause an error
@@ -20,23 +18,63 @@ void writeImage(const char * filepath, const char * outputPath, int blockSize)
   char * extension = fileExtension(outputPath);
 
   // check if output file format is supported
-  for (int i = 0; i < sizeof(supportedOutputFormats) / sizeof(char *); ++i) {
-
-    if (strcmp(extension, supportedOutputFormats[i]) == SUCCESS) {
-      free(extension);
-
-      ImageStbi * stbi = loadStbi(filepath);
-      if (!stbi) printCriticalError(ERROR_RUNTIME, "Error loading image [filepath: %s]", filepath);
-
-      unsigned char * asciiImage = stbiToAscii(stbi, blockSize);
-      if (!asciiImage) printCriticalError(ERROR_INTERNAL, "Error converting image to ascii format");
-      freeStbi(stbi);
-
-      asciiToPpm(asciiImage, outputPath);
-      return;
-    }
+  if (strcmp(extension, "ppm") == SUCCESS) 
+  {
+    free(extension);
+    writePpm(filepath, outputPath, blockSize);
+    return;
+  }
+  if (strcmp(extension, "txt") == SUCCESS) 
+  {
+    free(extension);
+    writeTxt(filepath, outputPath, blockSize);
+    return;
   }
 
   printCriticalError(ERROR_BAD_ARGUMENTS, "File format '%s' is not supported", extension);
-  //didn't forget "free(extension);", its just not needed as printCriticalError will stop control flow
+  //didn't forget "free(extension);", its just not needed as printCriticalError will stop control flow and OS will cleanup
+}
+
+void writePpm(const char * filepath, const char * outputPath, int blockSize)
+// convert image at filepath to ppm file
+{
+  if (!filepath) printCriticalError(ERROR_BAD_ARGUMENTS, "filepath is null pointer");
+  if (!outputPath) printCriticalError(ERROR_BAD_ARGUMENTS, "outputPath is null pointer");
+  if (blockSize < 1) printCriticalError(ERROR_BAD_ARGUMENTS, "blockSize < 1 [blockSize: %i]", blockSize);
+
+  // load stbi image and convert it to ascii
+  ImageStbi * stbi = loadStbi(filepath);
+  if (!stbi) printCriticalError(ERROR_RUNTIME, "Error loading image [filepath: %s]", filepath);
+
+  unsigned char * asciiImage = stbiToAscii(stbi, blockSize);
+  if (!asciiImage) printCriticalError(ERROR_INTERNAL, "Error converting image to ascii format");
+  
+  // write to file
+  asciiToPpm(asciiImage, outputPath);
+
+  freeStbi(stbi);
+  free(asciiImage);
+}
+
+void writeTxt(const char * filepath, const char * outputPath, int blockSize)
+// convert image at filepath to txt file
+{
+  if (!filepath) printCriticalError(ERROR_BAD_ARGUMENTS, "filepath is null pointer");
+  if (!outputPath) printCriticalError(ERROR_BAD_ARGUMENTS, "outputPath is null pointer");
+  if (blockSize < 1) printCriticalError(ERROR_BAD_ARGUMENTS, "blockSize < 1 [blockSize: %i]", blockSize);
+
+  // load stbi image and convert it to ascii image
+  ImageStbi * stbi = loadStbi(filepath);
+  if (!stbi) printCriticalError(ERROR_RUNTIME, "Error loading image [filepath: %s]", filepath);
+
+  unsigned char * asciiImage = stbiToAscii(stbi, blockSize);
+  if (!asciiImage) printCriticalError(ERROR_INTERNAL, "Error converting image to ascii format");
+  
+  // write to file
+  FILE * txt = fopen(outputPath, "w");
+  fprintf(txt, "%s", asciiImage);
+  fclose(txt);
+  
+  freeStbi(stbi);
+  free(asciiImage);
 }
--- src/writeFile.h
+++ src/writeFile.h
@@ -1,7 +1,11 @@
 #pragma once
 
-extern const char const * supportedOutputFormats[];
-
 void writeImage(const char * filepath, const char * outputPath, int blockSize);
 // convert image at filepath to ascii and write the resulting image in outputPath
 // if file format is not supported, function will cause an error
+
+void writePpm(const char * filepath, const char * outputPath, int blockSize);
+// convert image at filepath to ppm file
+
+void writeTxt(const char * filepath, const char * outputPath, int blockSize);
+// convert image at filepath to txt file
