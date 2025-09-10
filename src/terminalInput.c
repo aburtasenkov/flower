99a4b3b9e0eca8c2e4e9de365710c9a1ea1b36f3
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2025-09-10 02:45:48 +0200

    Better readability

--- src/terminalInput.c
+++ src/terminalInput.c
@@ -6,31 +6,28 @@
 #include <regex.h>
 #include <string.h>
 
+#define REGEX_FILE_EXTENSION_PATTERN "\\.([^.]+)$"
+
 char * fileExtension(const char * filepath)
 // return the file extension of filepath variable
 {
-  if (filepath == NULL) {
-    printf("Pre-condition fileExtension(const char * filepath): filepath is null pointer\n");
-    return NULL;
-  }
+  if (filepath == NULL) printCriticalError(ERROR_BAD_ARGUMENTS, "filepath is null pointer");
 
   regex_t regex;
   regmatch_t match[2];
 
   int statusCode = regcomp(&regex, REGEX_FILE_EXTENSION_PATTERN, REG_EXTENDED);
-  if (statusCode != SUCCESS) {
+  if (statusCode != 0) {
     char errorBuffer[128];
     regerror(statusCode, &regex, errorBuffer, sizeof(errorBuffer));
-    printf("Regex compilation failed: %s... Aborting\n", errorBuffer);
-    return NULL;
+    printCriticalError(ERROR_RUNTIME, "Regex compilation failed: %s", errorBuffer);
   }
 
   statusCode = regexec(&regex, filepath, 2, match, 0);
-  if (statusCode != SUCCESS) {
+  if (statusCode != 0) {
     char errorBuffer[128];
     regerror(statusCode, &regex, errorBuffer, sizeof(errorBuffer));
-    printf("Regex match failed: %s... Aborting\n", errorBuffer);
-    return NULL;
+    printCriticalError(ERROR_RUNTIME, "Regex match filed: %s", errorBuffer);
   }
 
   int start = match[1].rm_so;
@@ -38,11 +35,7 @@ char * fileExtension(const char * filepath)
 
   int length = end - start;
   char * extension = (char *)malloc(length + 1); // +1 for '\0'
-
-  if (!extension) {
-    printf("Error allocating memory for file extension. Aborting...\n");
-    return NULL;
-  }
+  if (!extension) printCriticalError(ERROR_RUNTIME, "Error allocating memory for file extension");
 
   memcpy(extension, filepath + start, length);
   extension[length] = '\0';
@@ -53,16 +46,18 @@ char * fileExtension(const char * filepath)
 }
 
 bool isVideo(const char * fileExtension) {
+  if (!fileExtension) printCriticalError(ERROR_BAD_ARGUMENTS, "fileExtension is null pointer");
   if (strcmp(fileExtension, "mp4") == SUCCESS) return true;
   return false;
 }
 
 bool isImage(const char * fileExtension) {
-  const char * ImageExtensionArray[] = {"jpg", "jpeg", "png", "bmp", "psd", "tga", "gif", "hdr", "pic", "ppm", "pgm"};
-  int sz = 11;  // size of ImageExtensionArray
+  static const char const * ImageExtensionArray[] = {"jpg", "jpeg", "png", "bmp", "psd", "tga", "gif", "hdr", "pic", "ppm", "pgm"};
+  size_t sz = 11;  // size of ImageExtensionArray
 
-  for (int i = 0; i < sz; ++i) {
-    if (strcmp(fileExtension, ImageExtensionArray[i]) == SUCCESS) return true;
+  for (size_t i = 0; i < sz; ++i) {
+    if (strcmp(fileExtension, ImageExtensionArray[i]) == 0) return true;
   }
+
   return false;
 }
--- src/terminalInput.h
+++ src/terminalInput.h
@@ -5,8 +5,6 @@
 #define ARGC_MIN 2
 #define MEDIAPATH_ARGV_INDEX 1
 
-#define REGEX_FILE_EXTENSION_PATTERN "\\.([^.]+)$"
-
 char * fileExtension(const char * filepath);
 // return the file extension of filepath variable
 
