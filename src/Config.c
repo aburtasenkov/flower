c590fec2fb46dd2c73cc57225ea859723738f096
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2025-09-10 02:41:01 +0200

    Updated for readability and consistency

--- src/Config.c
+++ src/Config.c
@@ -5,13 +5,13 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 OPTIONS loadDefaultConfig() {
-  static OPTIONS config;
-  config.filepath = NULL;
+  OPTIONS config = {0};
   config.FPS = 24;
   config.blockSize = 1; 
-  config.output = NULL;
   return config;
 }
 
@@ -19,16 +19,16 @@ void readTerminalArguments(OPTIONS * config, int argc, char ** argv) {
   if (argc < ARGC_MIN) printCriticalError(ERROR_BAD_ARGUMENTS, "Arguments not specified");
 
   config->filepath = argv[MEDIAPATH_ARGV_INDEX];
-  for (int i = MEDIAPATH_ARGV_INDEX + 1; i < argc; i++) {
-    if (strcmp(argv[i], "-f") == SUCCESS && i + 1 < argc) 
+  for (size_t i = MEDIAPATH_ARGV_INDEX + 1; i < argc; ++i) {
+    if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) 
     {
       config->FPS = atoi(argv[++i]);
     } 
-    else if (strcmp(argv[i], "-b") == SUCCESS && i + 1 < argc) 
+    else if (strcmp(argv[i], "-b") == 0 && i + 1 < argc) 
     {
       config->blockSize = atoi(argv[++i]);
     }
-    else if (strcmp(argv[i], "-o") == SUCCESS && i + 1 < argc) 
+    else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) 
     {
       config->output = argv[++i];
     }
