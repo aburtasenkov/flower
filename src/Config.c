1633b0bcd4c5aef1306a86c87dde2cc73fedaee8
aburtasenkov <aburtasenkov@gmail.com>
2025-09-14 02:15:04 +0200

    added preprocessors for readability

--- src/Config.c
+++ src/Config.c
@@ -11,6 +11,10 @@
 #define MIN_BLOCKSIZE 1
 #define MIN_FPS 0
 
+#define OPTION_FPS "-f"
+#define OPTION_BLOCKSIZE "-b"
+#define OPTION_OUTPUT "-o"
+
 OPTIONS loadDefaultConfig() {
   OPTIONS config = {0};
   config.FPS = 24;
@@ -23,19 +27,19 @@ void readTerminalArguments(OPTIONS * config, int argc, char ** argv) {
 
   config->filepath = argv[MEDIAPATH_ARGV_INDEX];
   for (size_t i = MEDIAPATH_ARGV_INDEX + 1; i < argc; ++i) {
-    if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) 
+    if (strcmp(argv[i], OPTION_FPS) == 0 && i + 1 < argc) 
     {
       int FPS = atoi(argv[++i]);
       if (FPS < MIN_FPS) printCriticalError(ERROR_BAD_ARGUMENTS, "FPS is smaller than 0 (unlimited)");
       config->FPS = (size_t)FPS;
     } 
-    else if (strcmp(argv[i], "-b") == 0 && i + 1 < argc) 
+    else if (strcmp(argv[i], OPTION_BLOCKSIZE) == 0 && i + 1 < argc) 
     {
       int blockSize = atoi(argv[++i]);
       if (blockSize < MIN_BLOCKSIZE) printCriticalError(ERROR_BAD_ARGUMENTS, "blockSize is smaller than 1"); 
       config->blockSize = (size_t)blockSize;
     }
-    else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) 
+    else if (strcmp(argv[i], OPTION_OUTPUT) == 0 && i + 1 < argc) 
     {
       config->output = argv[++i];
     }
