5b0326373417dce51d9ad967c841de90ec9bdd2d
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2025-09-20 22:02:09 +0200

    added const

--- src/ascii/stbiToAscii.c
+++ src/ascii/stbiToAscii.c
@@ -16,7 +16,7 @@ void init_grayscale_ascii_map()
     grayscale_ascii_map[i] = compute_grayscale_Char((uint8_t)i);
 }
 
-static inline char compute_grayscale_Char(uint8_t grayscale)
+static inline char compute_grayscale_Char(const uint8_t grayscale)
 // map a value of 0-255 on to an array of grayscale characters and return the character
 {
   static const char ascii[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
@@ -28,7 +28,7 @@ static inline char compute_grayscale_Char(uint8_t grayscale)
   return ascii[index];
 }
 
-static inline uint8_t rgb_grayscale_value(const unsigned char * pixel, size_t n_components) 
+static inline uint8_t rgb_grayscale_value(const unsigned char * pixel, const size_t n_components) 
 // convert single or multichannel pixel to grayscale value [0:255]
 {
   static const float RGB_FACTORS[] = {0.299f, 0.587f, 0.114f};
@@ -42,13 +42,13 @@ static inline uint8_t rgb_grayscale_value(const unsigned char * pixel, size_t n_
   return (uint8_t)grayscale;
 }
 
-static inline char grayscale_char(uint8_t grayscale)
+static inline char grayscale_char(const uint8_t grayscale)
 // retrieve pre computed ascii characters
 {
   return grayscale_ascii_map[grayscale];
 }
 
-char * stbi_to_ascii(const ImageStbi * stbi, size_t block_sz) 
+char * stbi_to_ascii(const ImageStbi * stbi, const size_t block_sz) 
 // create array of ascii characters
 // this array includes \n and \o for newlines and end of string
 // no checks and therefore the caller must ensure stbi is valid pointer
@@ -86,7 +86,7 @@ char * stbi_to_ascii(const ImageStbi * stbi, size_t block_sz)
   return ascii_image;
 }
 
-void print_image(const char * filepath, size_t block_sz) 
+void print_image(const char * filepath, const size_t block_sz) 
 // convert image located at filepath into ascii character array and print it out in terminal
 {
   if (!filepath) raise_critical_error(ERROR_BAD_ARGUMENTS, "filepath is null pointer");
--- src/ascii/stbiToAscii.h
+++ src/ascii/stbiToAscii.h
@@ -7,19 +7,19 @@
 void init_grayscale_ascii_map();
 // create hash map for each value [0:255] to avoid recalculating ascii characters with same grayscale value
 
-static inline char compute_grayscale_Char(uint8_t grayscale);
+static inline char compute_grayscale_Char(const uint8_t grayscale);
 // map a value of 0-255 on to an array of grayscale characters and return the character
 
-static inline uint8_t rgb_grayscale_value(const unsigned char * pixel, size_t n_components);
+static inline uint8_t rgb_grayscale_value(const unsigned char * pixel, const size_t n_components);
 // convert single or multichannel pixel to grayscale value [0:255]
 
-static inline char grayscale_char(uint8_t grayscale);
+static inline char grayscale_char(const uint8_t grayscale);
 // retrieve pre computed ascii characters
 
-char * stbi_to_ascii(const ImageStbi * stbi, size_t block_sz);
+char * stbi_to_ascii(const ImageStbi * stbi, const size_t block_sz);
 // create array of ascii characters
 // this array includes \n and \o for newlines and end of string
 // no checks and therefore the caller must ensure stbi is valid pointer
 
-void print_image(const char * filepath, size_t block_sz);
+void print_image(const char * filepath, const size_t block_sz);
 // convert image located at filepath into ascii character array and print it out in terminal
