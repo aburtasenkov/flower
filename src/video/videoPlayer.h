1e8432feb9122a2ea59ba1f22c924c280b3d6f8e
aburtasenkov <aburtasenkov@gmail.com>
2025-09-22 09:49:10 +0200

    forgot to remove parameters

--- src/video/videoPlayer.h
+++ src/video/videoPlayer.h
@@ -9,9 +9,9 @@ static void sleep_frame_time_offset(const struct timespec * start, const struct 
 // sleep until the next frame should be displayed
 // start and end are the times of the current frame processing
 
-static void print_ui(const ImageStbi * stbi, const size_t block_sz);
+static void print_ui();
 
-static void print_frame(const ImageStbi * stbi,  const size_t block_sz, const size_t fps);
+static void print_frame();
 
 void play_video(const char * filepath, const size_t block_sz);
 // convert a mp4 video into a sequence of frames in "frames" folder
