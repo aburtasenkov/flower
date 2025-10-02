4a9a1b0512b50e84e3025d02385cbaac490feb2e
aburtasenkov <aburtasenkov@gmail.com>
2025-10-02 21:40:58 +0200

    Removed arguments names'

--- src/io/files.h
+++ src/io/files.h
@@ -3,17 +3,17 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-bool file_exists(const char * filepath);
+bool file_exists(const char *);
 
-char * file_extension(const char * filepath);
+char * file_extension(const char *);
 // return the file extension of filepath variable
 
-void write_image(const char * filepath, const char * output_path, const size_t block_sz);
+void write_image(const char *, const char *, const size_t);
 // convert image at filepath to ascii and write the resulting image in output_path
 // if file format is not supported, function will cause an error
 
-static void write_ppm(const char * filepath, const char * output_path, const size_t block_sz);
+static void write_ppm(const char *, const char *, const size_t);
 // convert image at filepath to ppm file
 
-static void write_txt(const char * filepath, const char * output_path, const size_t block_sz);
+static void write_txt(const char *, const char *, const size_t);
 // convert image at filepath to txt file
--- src/io/terminal.h
+++ src/io/terminal.h
@@ -12,15 +12,15 @@
 #define CLEAR_COMMAND "clear"
 #endif
 
-int execute_command(const char * command);
+int execute_command(const char *);
 // Wrapper for system(*command*) calls in order to ease debugging
 
-bool is_video(const char * file_extension);
+bool is_video(const char *);
 
-bool is_image(const char * file_extension);
+bool is_image(const char *);
 
-void disable_raw_mode();
+void disable_raw_mode(void);
 // reset stdin flags
 
-void enable_raw_mode();
+void enable_raw_mode(void);
 // make stdin non blocking, disable canonical and echo modes
